0668d8f9580609555f094c27e75e3bd04e2d215f
xaviarazor <prietojavi551@gmail.com>
2019-10-24 08:07:39 -0500

    EnviarCorreo

---
+++ ProyectoTorneo/ProyectoTorneo/SnippetsUtiles.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Net.Mail;
+
+namespace ProyectoTorneo
+{
+    public class SnippetsUtiles
+    {
+        private void enviarCorreo() {
+            SmtpClient SmtpServer = new SmtpClient("smtp.office365.com");
+            SmtpServer.Port = 587;
+            SmtpServer.Credentials =
+            new System.Net.NetworkCredential("jprietoc@itam.mx", "XaviarToC551.");
+            SmtpServer.EnableSsl = true;
+
+            SmtpServer.Send("jprietoc@itam.mx", "queveduo@gmail.com", "Alguien entró al login de tu página!", "Yo!");
+        }
+    }
+}
